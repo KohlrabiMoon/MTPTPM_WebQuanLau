edc742ea8b451051ee8372d58a2edd83ffb97885
Nhat Nguyen <nhatcrys2000@gmail.com>
2023-04-09 16:38:14 +0700

    Update Last

---
+++ QuanLyQuanLau/BackEnd/HotPotWang/Test/Dangky.js
@@ -0,0 +1,18 @@
+const { isNumber } = require("lodash");
+const { isNull } = require("lodash");
+
+function Dangky(email,username,password){
+    if(isNull(email) || isNull(username) || isNull(password)){
+        throw new Error('Email hoặc Username hoặc Password không được Null');
+    }
+    else{
+        if(isNumber(email)){
+            throw new Error('Email không được là số');
+        }
+        else{
+            if(isNumber(username)){
+                throw new Error('Username không được là số');
+            }
+        }
+    }
+}
--- QuanLyQuanLau/BackEnd/HotPotWang/Test/operator.test.js
+++ QuanLyQuanLau/BackEnd/HotPotWang/Test/operator.test.js
@@ -1,6 +1,10 @@
 // const {Dangnhap} = require('./Dangnhap');
-
+const {Dangky} = require('./Dangky');
 
 // test('Taikhoansai',()=>{
 //     expect(Dangnhap('a','b')).toBe('Tài khoản đăng nhập sai')
 // })
+
+test('Khongnhapemail',()=>{
+    expect(Dangky('','a',)).toBe('Email hoặc Username hoặc Password không được Null')
+})
